66e333cb1cc2dd2b399bfb757f9b2caa858651c0
Charles Loehle <clguitar1@gmail.com>
2021-02-23 17:12:12 -0500

    finished layout

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./images\\bg-pattern-top-mobile.svg":[["bg-pattern-top-mobile.5debac92.svg","images/bg-pattern-top-mobile.svg"],"images/bg-pattern-top-mobile.svg"],"./images\\bg-pattern-bottom-mobile.svg":[["bg-pattern-bottom-mobile.e37cad24.svg","images/bg-pattern-bottom-mobile.svg"],"images/bg-pattern-bottom-mobile.svg"],"./images\\bg-pattern-top-desktop.svg":[["bg-pattern-top-desktop.eabc510c.svg","images/bg-pattern-top-desktop.svg"],"images/bg-pattern-top-desktop.svg"],"./images\\bg-pattern-bottom-desktop.svg":[["bg-pattern-bottom-desktop.f82eed7d.svg","images/bg-pattern-bottom-desktop.svg"],"images/bg-pattern-bottom-desktop.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./style.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50629" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52328" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
